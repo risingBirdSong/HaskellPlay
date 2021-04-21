f6bbb6781295ac0e3cc2115eb02d87eb55364a2d
peter <peter@Susans-MacBook-Air-2.local>
2021-04-20 23:09:38 -0700

    working and helping someone from chat over video on basic recursion, was good session... they started to get the hang of recursion which they had been struggling with

--- app/Main.hs
+++ app/Main.hs
@@ -4,3 +4,9 @@ import Play
 
 main :: IO ()
 main = someFunc
+
+isIsogram :: String -> Bool
+isIsogram = go []
+    where 
+    go l []     = True 
+    go l (x:xs) = if x `elem` l then False else go (x:l) xs
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,44 @@ module Lib
     ( someFunc
     ) where
 
+import Data.List
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+ourLength [] = 0
+ourLength [x] = 1
+ourLength (x:xs) = 1 + (ourLength xs) 
+
+ourLast [x] = x 
+ourLast (x:xs) = ourLast xs
+
+ourTake n _  
+    | n <= 0 = []
+ourTake _ [] = []
+ourTake n (x:xs) = x : (ourTake (n - 1) xs)
+
+ourMap f [] = []
+ourMap f (x:xs) = (f x) : (ourMap f xs )
+
+
+
+
+
+myMap f [] = []
+myMap f (x:xs) = (f x) : myMap f xs
+
+
+myLast (x:[]) = x 
+myLast (x:xs) = myLast xs
+
+
+
+myLength [] = 0 
+myLength (x:xs) = 1 + myLength xs
+
+
+myTake n _ | n <= 0 = []
+myTake _ [] = []
+myTake amount (x:xs) = x : (myTake (amount -1 ) xs) 
